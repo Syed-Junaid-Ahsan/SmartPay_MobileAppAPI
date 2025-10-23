18a61c2fab3ea188b891a2ad36194711868abc58
Syed-Junaid-Ahsan <jojiahsan.3@gmail.com>
2025-10-23 15:34:47 +0500

    Update .gitignore to exclude user and publish files

--- Services/Interfaces/ITokenService.cs
+++
@@ -1,7 +0,0 @@
-namespace SmartPayMobileApp_Backend.Services.Interfaces
-{
-    public interface ITokenService
-    {
-        string CreateToken(int userId, string email, string consumerNumber);
-    }
-}
